7493fec463ce6a6a9467994948339841602ba6dd
Simone Cociancich <sc@fabtotum.com>
2015-06-04 16:53:14 +0200

    250000 serial baud rate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -26,7 +26,7 @@
 
 // This determines the communication speed of the printer
 // This determines the communication speed of the printer
-#define BAUDRATE 115200
+#define BAUDRATE 250000
 
 // This enables the serial port associated to the Bluetooth interface
 //#define BTENABLED              // Enable BT interface on AT90USB devices
