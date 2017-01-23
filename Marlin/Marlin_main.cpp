a8c9d20a6d95f4472eb30f794e4623ed4ada9c9f
Simone Cociancich <sc@fabtotum.com>
2017-01-23 15:37:54 +0100

    Increased laser max temp a bit more for good measure

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -778,7 +778,7 @@ void FabtotumHeads_init ()
    available_heads[FAB_HEADS_laser_ID].mode = WORKING_MODE_LASER;
    available_heads[FAB_HEADS_laser_ID].heaters = 0;
    available_heads[FAB_HEADS_laser_ID].thtable = 3;
-   available_heads[FAB_HEADS_laser_ID].maxtemp = 70;
+   available_heads[FAB_HEADS_laser_ID].maxtemp = 80;
 
    tool_change(installed_head_id);
 }
